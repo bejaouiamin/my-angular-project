03ab3431c7a921d2d05cde283c0d4f9e96cda7ed
bejaouiamin <mb8080421@gmail.com>
2024-07-27 21:57:44 +0100

    fix the method login html and ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationRequest } from '../services/models/authentication-request';
+import { Router } from '@angular/router';
+import { AuthControllerService } from '../services/services/auth-controller.service';
+import { TokenService } from '../services/token/token.service';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor() { }
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
 
-  ngOnInit(): void {
+  constructor(   private router: Router,
+    private authService: AuthControllerService,
+    private tokenService: TokenService
+  ) {
+
   }
+
+    login() {
+      this.errorMsg = [];
+      this.authService.login({
+        body: this.authRequest
+      }).subscribe({
+        next: (res) => {
+          this.tokenService.token = res.token as string;
+          
+        },
+        error: (err) => {
+          console.log(err);
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.errorMsg);
+          }
+        }
+      });
+
+      }
 
 }
